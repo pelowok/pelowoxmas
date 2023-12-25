385b5d9f828cb6ba16946e28219f7b04a63a2871
pelowok <pelowok@users.noreply.github.com>
2023-12-25 01:44:30 -0500

    multichg

---
+++ script.js
@@ -0,0 +1,49 @@
+// Function to generate a set of random names
+function generateRandomNames() {
+  const adjectives = ['Happy', 'Funny', 'Brave', 'Clever', 'Gentle', 'Silly', 'Kind', 'Lively', 'Calm', 'Bright'];
+  const animals = ['Dog', 'Cat', 'Rabbit', 'Elephant', 'Lion', 'Tiger', 'Monkey', 'Horse', 'Dolphin', 'Penguin'];
+
+  const names = [];
+
+  for (const adjective of adjectives) {
+    for (const animal of animals) {
+      names.push(`${adjective} ${animal}`);
+    }
+  }
+
+  return names.sort(); // Sort the names alphabetically
+}
+
+// Function to check if the page exists
+async function checkPage(url) {
+  try {
+    const response = await fetch(url, { method: 'HEAD' });
+    return response.ok;
+  } catch (error) {
+    return false;
+  }
+}
+
+// Create links with pre-generated unique strings
+const gridContainer = document.getElementById('gridContainer');
+const names = generateRandomNames();
+
+for (const name of names) {
+  const link = document.createElement('a');
+  link.textContent = name;
+  const pageUrl = `./${name.toLowerCase()}.html`;
+  link.href = pageUrl;
+
+  const gridItem = document.createElement('div');
+  gridItem.classList.add('grid-item');
+  gridItem.appendChild(link);
+
+  // Check if the page exists
+  checkPage(pageUrl).then(exists => {
+    if (!exists) {
+      link.href = './tryagain.html'; // Redirect to tryagain.html if page not found
+    }
+  });
+
+  gridContainer.appendChild(gridItem);
+}
